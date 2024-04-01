4fa1ce940217a4cb45c730f73670805d146d40b3
juliamblake1 <91092063+juliamblake1@users.noreply.github.com>
2024-04-01 12:27:05 -0400

    Starting R code

---
+++ census_data_1.R
@@ -0,0 +1,17 @@
+# NOTE: To load data, you must download both the extract's data and the DDI
+# and also set the working directory to the folder with these files (or change the path below).
+
+#setwd("C:/IPUMS")
+#install.packages("ipumsr")
+#library(ipumsr)
+
+ddi <- read_ipums_ddi("usa_00001.xml")
+data <- read_ipums_micro(ddi)
+
+data_df <- as.data.frame(data)
+head(data_df)
+
+
+summary(data_df)
+str(data_df)
+head(data_df)
