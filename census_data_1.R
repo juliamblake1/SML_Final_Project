849b4571343ac26162ae8fed6cd58ba798df2a6c
juliamblake1 <91092063+juliamblake1@users.noreply.github.com>
2024-04-02 10:34:15 -0400

    Add files via upload

--- census_data_1.R
+++ census_data_1.R
@@ -5,7 +5,7 @@
 #install.packages("ipumsr")
 #library(ipumsr)
 
-ddi <- read_ipums_ddi("usa_00001.xml")
+ddi <- read_ipums_ddi("usa_00002.xml")
 data <- read_ipums_micro(ddi)
 
 data_df <- as.data.frame(data)
